c77b2c68fcddd475497ab95544c48e95e94988d0
ezomo <ezomo1daniel@gmail.com>
2024-08-24 14:29:40 +0000

    調教

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use safe_drive::{
+    context::Context, error::DynError, logger::Logger, msg::common_interfaces::geometry_msgs::msg,
+    msg::common_interfaces::sensor_msgs,
+};
+
+use differential_two_wheel_control::DtwcSetting;
+use motor_controller::udp_communication;
+
+const ROBOT_CENTER_TO_WHEEL_DISTANCE: f64 = 0.37;
+
+fn main() -> Result<(), DynError> {
+    let dtwc_setting = DtwcSetting {
+        l_id: 0,
+        r_id: 1,
+        robot_center_to_wheel_distance: ROBOT_CENTER_TO_WHEEL_DISTANCE,
+    };
+
+    // for debug
+    let _logger = Logger::new("robo2_3_2024_a");
+    let ctx = Context::new()?;
+    let mut selector = ctx.create_selector()?;
+    let node = ctx.create_node("robo2_3_2024_a", None, Default::default())?;
+
+    let subscriber_cmd = node.create_subscriber::<msg::Twist>("cmd_vel2_3", None)?;
+    let subscriber_joy = node.create_subscriber::<sensor_msgs::msg::Joy>("rjoy_2_3", None)?;
+
+    selector.add_subscriber(
+        subscriber_cmd,
+        Box::new(move |msg| {
+            let motor_power = dtwc_setting.move_chassis(msg.linear.x, msg.linear.y, msg.angular.z);
+
+            for i in motor_power.keys() {
+                udp_communication::send_pwm_udp("50007", "192.168.4:60000", *i, motor_power[i]);
+            }
+        }),
+    );
+
+    selector.add_subscriber(
+        subscriber_joy,
+        Box::new(move |_msg| {
+            todo!();
+        }),
+    );
+
+    loop {
+        selector.wait()?;
+    }
 }
